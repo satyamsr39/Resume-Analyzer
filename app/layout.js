d63ad2c8b57638c2bc51555e1ce2f33cff8c4f4f
satyam <sars@gmail.com>
2025-07-07 09:05:17 +0530

    Analytics added

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css'
+import { Analytics } from "@vercel/analytics/next"
 import Navbar from './components/Navbar';
 
 
@@ -8,7 +9,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
     <body className="bg-gray-50 text-gray-800">
       <Navbar/>
-       <main className="max-w-5xl mx-auto p-4">{children}</main> 
+       <main className="max-w-5xl mx-auto p-4">{children}</main>
+       <Analytics/> 
       
       </body>
     </html>
